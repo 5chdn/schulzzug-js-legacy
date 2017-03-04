068b0d4e955d67d086e060d0affbfb2f7688ba0d
Benjamin Maier <benjaminfrankmaier@gmail.com>
2017-03-04 22:45:41 +0100

    with comments

--- object_movement_test.js
+++ object_movement_test.js
@@ -3,67 +3,81 @@
 var width = 375;
 var height = 667;
 
+// object to be displayed (coin and such)
 var object;
 
+// position of horizon on y-axis
 var horizont = height-210;
+
+// Schulzzuggeschwindigkeit
 var v = 10;
+
+// distance to horizon
 var L = 40000;
+
+// height of camera
 var h_camera = 50;
+
+// x position of camera
 var x_camera = width/2;
 
+// distances of rails
 var gleisabstand_innen = 10;
 var gleisabstand_aussen = 6;
 
+// height/width of test object
 var h_object = gleisabstand_innen;
 var w_object;
+
+//start coordinate of test object
 var x_s = width/2 - gleisabstand_aussen-gleisabstand_innen;
-//var x_s = width/2;
-var gleis_lines = Array();
+
+// graphics object for lines
+var gfx;
 
 // game engine
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update });
 
+// after that time, movement will start
 var t0;
-// global vars
-//
+
 function preload() {
     game.load.image('dummy','assets/1pixel.png');
 }
 
-var line1;
-var gfx;
 var original_object_height;
 // create scenery
 function create() {
 
-    //scale to screen of window
-    //game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    //game.scale.pageAlignHorizontally = true;
-    //game.scale.pageAlignVertically = true;
-    //game.scale.setScreenSize(true);
-
-    //game.stage.backgroundColor = "#FFFFFF";
-
-    //background picture
-    //bg = game.add.sprite(0,0,'bg');
-    //bg.scale.setTo(1.6,1.6);
+    //add for gleise
     gfx = game.add.graphics(0, 0); 
     gfx.lineStyle(1, 0xff0000, 1);
 
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     //schulzzug
+    //
+    //
+    //add test object
     object = game.add.sprite(0,0,'dummy');
     object.anchor.setTo(0.5,0.5);
+
+    //set start x-value
     object.x = x_s;
+    //flip_z is necessary due to different orientation of screen coordinates
     object.y = flip_z(horizont + h_object/2);
+
+    //get the original height of the object to scale it to the wanted heifht
     original_object_height = object.height;
+
+    //get and set new scale
     var new_scale = h_object / original_object_height;
     object.scale.setTo(new_scale,new_scale);
     w_object = object.width;
     
     draw_gleis(); 
 
+    //set object start time to now
     t0 = game.time.now;
 
 }
@@ -104,24 +118,34 @@ function draw_gleis() {
 
 function update() {
 
+    //get current time
     t = game.time.now;
-    var y = v*(t-t0);
-    var x_o = x_camera - L/(L-y) * (x_camera - x_s);
-    var x_L = x_camera - L* (x_camera - (x_s - w_object/2))/(L - y);
-    var x_R = x_camera - L* (x_camera - (x_s + w_object/2))/(L - y);
-    var w = x_R - x_L;
 
+    //get position between horizon and camera
+    var y = v*(t-t0);
+
+    //get center position of test object
+    var x_o = x_camera - L/(L-y) * (x_camera - x_s);
+
+    //set center position of object
     object.x = x_o;
+
+    //get new width
     var w = -L*( (x_camera-(x_s+w_object/2))/(L-y) - (x_camera - (x_s-w_object/2))/(L-y) );
+
+    //get and set new scale of object
     var wScale = w/original_object_height;
     object.scale.setTo(wScale);
+
+    //get vertical coordinate
     var h = h_camera - L/(L-y) * (h_camera - h_object / 2.) + horizont;
     object.y = flip_z(h);
-
+    
+    //destroy if out of scope
     if (object.y-object.height/2. > height) 
-        {
-            object.destroy()
-        }
+    {
+        object.destroy()
+    }
     
 }
 
