ef0897a6c9b28b06146c286cd7000d469323441c
Benjamin Maier <benjaminfrankmaier@gmail.com>
2017-03-14 00:37:11 +0100

    fix #50

--- public/js/boot.js
+++ public/js/boot.js
@@ -6,6 +6,7 @@ let boot_state = {
     create: function () {
 
         // start physics enginge and continue loading all assets
+        // game.stage.disableVisibilityChange = true;
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.state.start("load");
     }
