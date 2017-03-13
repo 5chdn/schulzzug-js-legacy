a35b24358b57f421b4d9918a4064a0c955dc9883
5chdn <5chdn@users.noreply.github.com>
2017-03-13 21:05:27 +0100

    Add firebase js

---
+++ public/js/firebase.js
@@ -0,0 +1,61 @@
+// CONFIGURATION
+var config = {
+    apiKey: "AIzaSyAo0p0M13L2_zPK-YJ5IITd-WczdJTUFPA",
+    authDomain: "schulzzug-b84fc.firebaseapp.com",
+    databaseURL: "https://schulzzug-b84fc.firebaseio.com",
+};
+firebase.initializeApp(config);
+
+firebase.auth().signInAnonymously().then(function() {
+  	configurePresence();
+}).catch(function(error) {
+  	console.log(error.message);
+});
+
+function configurePresence() {
+	var connections = firebase.database().ref('connections');
+	var connectedRef = firebase.database().ref('.info/connected');
+	connectedRef.on('value', function(snap) {
+  		if (snap.val() === true) {
+	  		var con = connections.push(true);
+    		con.onDisconnect().remove();
+  		}
+	});
+}
+
+// EXPORTED FUNCTIONS
+function updateGameResult(score, distance) {
+	var userId = firebase.auth().currentUser.uid;
+	var timestamp = firebase.database.ServerValue.TIMESTAMP;
+
+	var gameResult = {
+		score: score,
+		distance: distance,
+		timestamp: timestamp
+	}
+
+	var newGameResultKey = firebase.database().ref().child('game_results').push().key;
+
+	var updates = {}
+	updates['/game_results/' + newGameResultKey] = gameResult;
+	updates['/user-game_results/' + userId + '/' + newGameResultKey] = gameResult;
+
+	return firebase.database().ref().update(updates)
+}
+
+function fetchActiveUserCount(callback) {
+	var connections = firebase.database().ref('connections');
+	connections.on('value', function(snapshot) {
+		var count = snapshot.numChildren();
+		callback(count);
+	});
+}
+
+function fetchGameStats(callback) {
+	var gameResultsRef = firebase.database().ref('stats');
+	gameResultsRef.once('value', function(snapshot) {
+        var newTotalScore = snapshot.val().totalScore;
+		var newTotalDistance = snapshot.val().totalDistance;
+		callback(newTotalScore, newTotalDistance);
+	});
+}
