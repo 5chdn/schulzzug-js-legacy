e920941e75bfe4de159ba3318eae6de34e2efe8f
Benjamin Maier <benjaminfrankmaier@gmail.com>
2017-03-13 13:24:56 +0100

    changed ende font

--- js/ende.js
+++ js/ende.js
@@ -6,8 +6,9 @@ var ende_state = {
             font:'30px SilkScreen monospace',
             fill: 'white'
         }
-        let loading_label = game.add.text(canvas_width/2, canvas_height/2,'Ende.',style) ;
-        loading_label.anchor.setTo(0.5,0.5);
+        let ende_label = game.add.text(canvas_width/2, canvas_height/2,'Ende.',style) ;
+        ende_label.font = 'SilkScreen';
+        ende_label.anchor.setTo(0.5,0.5);
     }
 
 }
