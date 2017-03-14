49c5f4aa99f37b05ebb85b0ec08633263f33bafa
Benjamin Maier <benjaminfrankmaier@gmail.com>
2017-03-14 18:36:27 +0100

    added fading state function

--- public/js/game.js
+++ public/js/game.js
@@ -20,3 +20,48 @@ game.state.add("end",end_state);
 
 // starting the game
 game.state.start("boot");
+
+function fade(in_or_out,callback,transition_duration,state_key) {
+
+    let alpha_start, alpha_end;
+
+    if (in_or_out == null)
+        throw "Tell me to either fade 'in' or 'out', null is invalid";
+    else if (in_or_out == "in"){
+        alpha_start = 1;
+        alpha_end = 0;
+    } else if (in_or_out == "out"){
+        alpha_start = 0;
+        alpha_end = 1;
+    } else
+        throw "Not sure what to do with " + in_or_out;
+
+    if (transition_duration == null) {
+        transition_duration = state_transition_duration;
+    }
+
+    let rect = game.add.sprite(0,0,"black");
+    rect.width = canvas_width;
+    rect.height = canvas_height;
+    rect.alpha = alpha_start;
+
+
+    let fade_out = game.add.tween(rect).to(
+                                            {
+                                                alpha: alpha_end
+                                            },
+                                            state_transition_duration,
+                                            Phaser.Easing.Linear.None
+                                          );
+    fade_out.onComplete.add( function (){
+        if (in_or_out == "out")
+            game.state.start(state_key);
+
+        rect.destroy();
+        if (callback !== null)
+            callback();
+    });
+
+    fade_out.start();
+}
+
